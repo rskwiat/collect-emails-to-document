1e855c7e39ad1b154d702d51b36713685661dfb4
Robert Skwiat <rskwiat@gmail.com>
2020-05-13 10:06:09 -0400

    updating email script adding in regex checking and clearing email file

--- src/routes/email.js
+++ src/routes/email.js
@@ -4,11 +4,21 @@ const fs = require('fs')
 
 router.post('/email', (req, res) => {
   const { email } = req.body;
+
+  //@todo - move to seperate function
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const test = re.test(String(email).toLowerCase());
+
+  if (!test) {
+    return res.status(422).send({ error: 'You must provide a valid email address' });
+  }
+
   try {
     fs.writeFileSync('data/emails.txt', email + '\r\n', { flag: 'a+' });
-    res.end();
+    return res.status(200).end({ success: 'Email was saved' });
   } catch (err) {
-    console.error(err)
+    res.status(422).send({ error: err });
   }
 });
 
